6028fcfe372f7e288d5a9c75a1abba7556257fb7
Matheus Martins <mail@mmartins.dev>
2024-10-19 23:46:55 -0300

    first commit

--- src/main/java/dev/mmmartins/agendaservico/repository/UsuarioRepository.java
+++ src/main/java/dev/mmmartins/agendaservico/repository/UsuarioRepository.java
@@ -4,9 +4,11 @@ import dev.mmmartins.agendaservico.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findAllByOrderByTipoAcessoAsc();
     Optional<Usuario> findByUsuarioIgnoreCase(String username);
 }
