4ddcfa9b454dbdcc26e31eb812b9c2641f41d447
Matheus Martins <mail@mmartins.dev>
2024-11-04 00:01:40 -0300

    add relatorios-api

---
+++ relatorios-api/src/main/java/dev/mmmartins/relatoriosapi/RelatoriosApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mmmartins.relatoriosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatoriosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelatoriosApiApplication.class, args);
+    }
+
+}
---
+++ relatorios-api/src/test/java/dev/mmmartins/relatoriosapi/RelatoriosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mmmartins.relatoriosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatoriosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
